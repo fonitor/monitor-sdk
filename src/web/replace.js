d55332aee568ad1ab93339408a7977ef35f2df75
xiaolin <462441355@qq.com>
2021-03-12 16:27:12 +0800

    * sdk

--- src/web/replace.js
+++ src/web/replace.js
@@ -302,17 +302,21 @@ export function performanceReplace() {
             console.log('你的浏览器不支持 performance 接口');
             return;
         }
-        // let times = performance.timing.toJSON()
-        // 发送页面性能指标数据, 上报内容 => 
-        // let perf = this._objectSpread({}, times, {
-        //     url: "".concat(window.location.host).concat(window.location.pathname)
-        // })
-        // 页面耗时
-        // console.log(perf)
-        // 资源耗时
-        // console.log(performance.getEntries())
-        // 浏览器内存情况 usedJSHeapSize表示所有被使用的js堆栈内存；totalJSHeapSize表示当前js堆栈内存总大小，这表示usedJSHeapSize不能大于totalJSHeapSize，如果大于，有可能出现了内存泄漏。
-        // console.log(performance.memory)
+        let timing = performance.timing;
+        console.log('准备新页面时间耗时: ' + timing.fetchStart - timing.navigationStart);
+        console.log('redirect 重定向耗时: ' + timing.redirectEnd - timing.redirectStart);
+        console.log('Appcache 耗时: ' + timing.domainLookupStart - timing.fetchStart);
+        console.log('unload 前文档耗时: ' + timing.unloadEventEnd - timing.unloadEventStart);
+        console.log('DNS 查询耗时: ' + timing.domainLookupEnd - timing.domainLookupStart);
+        console.log('TCP连接耗时: ' + timing.connectEnd - timing.connectStart);
+        console.log('request请求耗时: ' + timing.responseEnd - timing.requestStart);
+        console.log('白屏时间: ' + timing.responseStart - timing.navigationStart);
+        console.log('请求完毕至DOM加载: ' + timing.domInteractive - timing.responseEnd);
+        console.log('解释dom树耗时: ' + timing.domComplete - timing.domInteractive);
+        console.log('从开始至load总耗时: ' + timing.loadEventEnd - timing.navigationStart);
+        // 首屏时间
+        // domLoadedTime = timing.domContentLoadedEventStart - timing.navigationStart 
+
     }
 
 }
